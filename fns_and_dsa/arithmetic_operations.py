1b40fb090ccc06df77a44d965d3bc27d36b82616
Derilee <victorsamuelpraise@gmail.com>
2024-07-07 15:26:38 +0000

    arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,4 +10,4 @@ def perform_operation(num1, num2, operation):
             if num1 == 0 or num2 == 0:
                 return("can't divide by zero")
             else:
-                return num1 // num2
+                return num1 / num2
