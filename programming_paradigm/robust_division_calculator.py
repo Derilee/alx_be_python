83b728ce2a4469159ff77f87633e1b24b27ccf82
Derilee <victorsamuelpraise@gmail.com>
2024-07-18 21:54:02 +0000

    Robust Division Calculator with Command Line Arguments

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,6 +3,7 @@ def safe_divide(numerator, denominator):
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         elif numerator != 0 or denominator !=0:
-            print(f"The result of the division is {numerator/denominator}")
+            result = float(numerator)/float(denominator)
+            print(f"The result of the division is {result}")
     except ValueError:
         print("Error: Please enter numeric values only.")
